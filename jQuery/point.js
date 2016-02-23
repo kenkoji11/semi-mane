29f2ba023941ac2ba5d0dba06e073e360ad82385
kenkoji11 <timeismoney11>
2016-02-23 19:59:43 +0900

    start

---
+++ jQuery/point.js
@@ -0,0 +1,24 @@
+<<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>得点入力画面</title>
+      <link href="css/bootstrap.css" rel="stylesheet">
+</head>
+<body>
+<h1>得点入力画面</h1>
+<hr>
+<select name="jitsu" size="5">
+<option value="---">実現性</option>
+<option value="point5">5</option>
+<option value="point4">4</option>
+<option value="point3">3</option>
+<option value="point2">２</option>
+<option value="point1">１</option>
+</select>
+<div class="container">
+  <div class="well">
+    <button type="button" class="btn btn-default">送信</button>
+
+</body>
+</html>
